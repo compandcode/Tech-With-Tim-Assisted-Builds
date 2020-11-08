56a0fbc8fb13be6adeef144845141af4e58ea20d
compandcode <66060875+vncomp@users.noreply.github.com>
2020-11-08 15:34:15 +0000

    Completed Video 2 of the series.

--- Flask Tutorial/FlaskTut.py
+++ Flask Tutorial/FlaskTut.py
@@ -1,19 +1,10 @@
-from flask import Flask, redirect, url_for
-#https://www.youtube.com/playlist?list=PLzMcBGfZo4-n4vJJybUVV3Un_NFS5EOgX Start with 2 next.
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__) #Creates instance of webpage.
 
-@app.route("/")
-def home():
-    return "Hello, this is the homepage <h1>Hello World</h1>"
+@app.route("/<name>") #Defines the landing page.
+def home(name):
+    return render_template("index.html",content=["King","CompAndCode"],r=2)
 
-@app.route("/about")
-def about():
-    return "About <strong>CompAndCode</strong>"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("home")) #Redirects to the homepage.
-
-if __name__ == "__main__":
+if __name__ == "__main__": #Always runs.
     app.run()
