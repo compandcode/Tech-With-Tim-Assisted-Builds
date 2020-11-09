d3c9c830aee19a386c62e756c3824e8bbb0dc750
compandcode <66060875+vncomp@users.noreply.github.com>
2020-11-09 18:54:05 +0000

    Added 5 class attributes to Room. Now working on methods.

--- ReactDjango/music_controller/api/models.py
+++ ReactDjango/music_controller/api/models.py
@@ -4,3 +4,15 @@ from django.db import models
 
 #Django Rule: Fat models, thin views,
 
+
+class Room(models.Model):  # The Room is going to be a model.
+    #Class attributes for each room:
+    # Unique code for each room (characters). Constraints shown.
+    code = models.CharField(max_length=8, default="", unique=True)
+    # Stores information relating to the host of the room. Keeps track of them.
+    host = models.CharField(max_length=50, unique=True)
+    # States by default they shouldn't be able to pause.
+    guest_can_pause = models.BooleanField(null=False, default=False)
+    votes_to_skip = models.IntegerField(null=False, default=1)
+    # Date/Time is automatically added, we don't need to pass it.
+    created_at = models.DateTimeField(auto_now_add=True)
