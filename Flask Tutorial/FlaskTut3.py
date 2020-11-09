bcd55a6a4ee20f0293d26cb562bbf71a5aab099d
compandcode <66060875+vncomp@users.noreply.github.com>
2020-11-09 14:53:37 +0000

    Completed Video 3 of the series.
    Added the Bootstrap NavBar.
    Learnt about inheritance (new page inherits the header from the base page).
    Towards the end, I will use the skills to create a custom project.

--- Flask Tutorial/FlaskTut3.py
+++ Flask Tutorial/FlaskTut3.py
@@ -1,10 +1,15 @@
 from flask import Flask, redirect, url_for, render_template
+#Focussing on Bootstrap and Template Inheritance.
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html", content="Testing")
+    return render_template("index.html")
+
+@app.route("/test")
+def test():
+    return render_template("new.html") #Nav bar stays because it's inherited.
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True) #Means the server doesn't have to be rerun each time a page is changed.
