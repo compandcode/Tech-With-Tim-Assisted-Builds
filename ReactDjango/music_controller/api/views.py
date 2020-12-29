d72c620e555db1d0d1bef0ef67182603bf05c179
compandcode <66060875+vncomp@users.noreply.github.com>
2020-12-29 15:32:09 +0000

    Completed Video 2!

--- ReactDjango/music_controller/api/views.py
+++ ReactDjango/music_controller/api/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .serializers import RoomSerializer
+from .models import Room
 
 # Create your views here.
-def main(request):
-    return HttpResponse("<h1>Hello</h1>")
+class RoomView(generics.ListAPIView): #Blueprint for viewing rooms and creating them. *****
+    querySet = Room.objects.all() #Stores all the rooms.
+    serializer_class = RoomSerializer #Converts the rooms into a more comprehendable format.
+
